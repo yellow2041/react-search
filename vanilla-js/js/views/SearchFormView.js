f82945b5414be81c4858af47de84a45698c01b92
yellow2041 <jeeyeon654@gmail.com>
2024-06-14 00:01:51 +0900

    feat/검색어 입력시 리셋버튼 표시

--- vanilla-js/js/views/SearchFormView.js
+++ vanilla-js/js/views/SearchFormView.js
@@ -1,15 +1,26 @@
-import { qs } from "../helpers.js";
+import { on, qs } from "../helpers.js";
 import View from "./View.js";
 
 export default class SearchFormView extends View {
   constructor() {
     super(qs("#search-form-view"));
     this.resetElement = qs("[type=reset]", this.element);
-    console.log(this.resetElement);
+    this.inputElement = qs("[type=text]", this.element);
+
     this.showResetButton(false);
+    this.bindEvent();
   }
 
   showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
+  }
+
+  bindEvent() {
+    on(this.inputElement, "keyup", () => this.handleKeyup());
+  }
+
+  handleKeyup() {
+    const { value } = this.inputElement;
+    this.showResetButton(value.length > 0);
   }
 }
