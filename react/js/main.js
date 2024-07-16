939b1428ec73e74bfc0100e0905d01e3e6e1f5b1
yellow2041 <jeeyeon654@gmail.com>
2024-07-16 21:03:31 +0900

    검색폼 입력 및 초기화 구현

--- react/js/main.js
+++ react/js/main.js
@@ -1,6 +1,60 @@
-const element = (
-  <header>
-    <h2 className="container">검색</h2>
-  </header>
-);
-ReactDOM.render(element, document.querySelector("#app"));
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchKeyword: "",
+    };
+  }
+  handleChangeInput(event) {
+    const searchKeyword = event.target.value;
+
+    if (searchKeyword <= 0) return this.handleReset();
+
+    this.setState({
+      searchKeyword,
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+  handleReset() {
+    this.setState(
+      () => {
+        return {
+          searchKeyword: "",
+        };
+      },
+      () => console.log(this.state.searchKeyword)
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <h2 className="container">검색</h2>
+        </header>
+        <div className="container">
+          <form
+            onSubmit={(event) => this.handleSubmit(event)}
+            onReset={() => this.handleReset()}
+          >
+            <input
+              type="text"
+              placeholder="검색어를 입력하세요"
+              autoFocus
+              value={this.state.searchKeyword}
+              onChange={(event) => this.handleChangeInput(event)}
+            />
+            {this.state.searchKeyword.length > 0 && (
+              <button type="reset" className="btn-reset"></button>
+            )}
+          </form>
+        </div>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
