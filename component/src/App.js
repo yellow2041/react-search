6593bde2b12ae24e38b38bd3f1cf3dc00b9fe710
yellow2041 <jeeyeon654@gmail.com>
2024-08-20 21:48:59 +0900

    SearchForm 구현

--- component/src/App.js
+++ component/src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
 import Header from "./components/Header.js";
+import SearchForm from "./components/SearchForm.js";
 
 class App extends React.Component {
+  search(searchKeyword) {
+    console.log(searchKeyword);
+  }
+  handleReset() {
+    console.log("Reset");
+  }
   render() {
-    return <Header title="검색" />;
+    return (
+      <>
+        <Header title="검색" />
+        <div class="container">
+          <SearchForm
+            onSubmit={(searchKeyword) => this.search(searchKeyword)}
+            onReset={() => this.handleReset()}
+          />
+        </div>
+      </>
+    );
   }
 }
 
